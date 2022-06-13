b1e97e7d0c9d35d4fe7d896ce7d1af86270b09c2
NaufalK25 <naufalkateni2001@gmail.com>
2022-06-13 16:21:45 +0700

    add dev branch

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
+
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
