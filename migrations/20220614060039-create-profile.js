9cb7ea7d9e0a51dfa2eb58bb1bc7004d8a1d5ab7
NaufalK25 <naufalkateni2001@gmail.com>
2022-06-14 17:27:28 +0700

    Revert "remove profile model"
    This reverts commit 075fd1688ee7639d2c34bd5ffc8e5d3261cd1526.

---
+++ migrations/20220614060039-create-profile.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      profilePicture: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Cities',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      address: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
