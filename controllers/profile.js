a106f44b7eee1b77cf8ba9b2c90425c7c882f5d7
LuthfiyahS <luthfiyah.sakinah19@gmail.com>
2022-06-14 20:52:08 +0700

    Update Info Profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,66 +1,79 @@
 const fs = require('fs');
 const { validationResult } = require('express-validator');
-const { Profile } = require('../models');
+const { User, Profile } = require('../models');
 
 module.exports = {
-  updateProfile: async (req, res) => {
-    const errors = validationResult(req);
-
-    const profile = await Profile.findByPk(req.body.userId);
-    const updatedData = {};
-    const profilePicture = req.file ? req.file.filename : 'default-profile.png';
-
-    if (!profile){
-        res.status(404).json({
-            success: false,
-            message: 'Profile is not found',
-            data: null
-        });
-    }
-
-    if (Object.keys(req.body).length > 0) {
-        //const { userId, name, phoneNumber, cityId, address } = req.body;
-        const { userId, name, phoneNumber, address } = req.body;
-        if (profile.userId !== userId) updatedData.userId = userId;
-        if (profile.name !== name) updatedData.name = name;
-        if (profile.phoneNumber !== phoneNumber) updatedData.phoneNumber = phoneNumber;
-        //if (profile.cityId !== cityId) updatedData.cityId = cityId;
-        if (profile.address !== +address) updatedData.address = +address;
-    }
-    if (req.file && profilePicture !== 'default-profile.png') {
-        updatedData.profilePicture = profilePicture;
-        fs.unlink(`${unlinkProfilePicturePath}${profile.profilePicture}`, err => {
-            if (err) {
-                return res.status(500).json({
-                    success: false,
-                    message: 'Internal Server Error',
-                    data: null
-                });
-            }
-        });
-    }
-
-    if (Object.keys(updatedData).length === 0) {
-        return res.status(200).json({
-            success: true,
-            message: 'No changes has been made',
+    findAll: async (req, res) => {
+        //const profile = await Profile.findAll({ include: [{ model: User }] });
+        const profile = await Profile.findAll();
+        res.status(200).json({
+            statusCode: 200,
+            message: 'OK',
             data: profile
         });
-    }
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
+    },
+    findByPk: async (req, res) => {
+
+    },
+    update: async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return badRequest(errors.array(), req, res);
+
+        const profile = await Profile.findByPk(req.body.userId);
+        const updatedData = {};
+        const profilePicture = req.file ? req.file.filename : 'default-profile.png';
+
+        if (!profile) {
+            res.status(404).json({
+                success: false,
+                message: 'Profile is not found',
+                data: null
+            });
+        }
+
+        if (Object.keys(req.body).length > 0) {
+            //const { userId, name, phoneNumber, cityId, address } = req.body;
+            const { userId, name, phoneNumber, address } = req.body;
+            if (profile.userId !== userId) updatedData.userId = userId;
+            if (profile.name !== name) updatedData.name = name;
+            if (profile.phoneNumber !== phoneNumber) updatedData.phoneNumber = phoneNumber;
+            //if (profile.cityId !== cityId) updatedData.cityId = cityId;
+            if (profile.address !== +address) updatedData.address = +address;
+        }
+        if (req.file && profilePicture !== 'default-profile.png') {
+            updatedData.profilePicture = profilePicture;
+            fs.unlink(`${unlinkProfilePicturePath}${profile.profilePicture}`, err => {
+                if (err) {
+                    return res.status(500).json({
+                        success: false,
+                        message: 'Internal Server Error',
+                        data: null
+                    });
+                }
+            });
+        }
+
+        if (Object.keys(updatedData).length === 0) {
+            return res.status(200).json({
+                success: true,
+                message: 'No changes has been made',
+                data: profile
+            });
+        }
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: true,
+                message: 'No changes has been made',
+                data: errors.array()
+            });
+        }
+
+        await Profile.update(updatedData, { where: { id: req.body.userId } });
+
+        res.status(201).json({
             success: true,
-            message: 'No changes has been made',
-            data: errors.array()
+            message: 'Update Profile successful',
+            data: profile
         });
-    }
-
-    await Profile.update(updatedData, { where: { id: req.body.userId } });
-
-    res.status(201).json({
-        success: true,
-        message: 'Update Profile successful',
-        data:  profile 
-    });
     },
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,71 @@
-const { Router } = require('express');
-const { body } = require('express-validator');
-const { updateProfile} = require('../controllers/profile');
-//const { Profile } = require('../models');
+const express = require('express');
+const multer = require('multer');
+const { body, param } = require('express-validator');
+const passport = require('../middlewares/passport');
+const { forbidden, internalServerError, methodNotAllowed,
+  notFound, unAuthorized } = require('../controllers/error');
+const { findAll,update} = require('../controllers/profile');
+const { Profile } = require('../models');
+const { profileStorage } = require('../middlewares/file');
 
-const router = Router();
+
+const router = express.Router();
+
+//router.get('/view-profile',findAll);
+router.route('/view-profile').get(
+  (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, async (err, user, info) => {
+        if (err) return internalServerError(err, req, res);
+        if (!user) return unAuthorized(req, res);
+        if (user.roleId === 1) return next();
+        const profile = await Profile.findByPk(req.query.id);
+        if (!profile) return notFound(req, res);
+        if (user.id !== req.body.id) return forbidden(req, res);
+        next();
+    })(req, res, next)},findAll)
+    
+router.route('/info-profile')
+    // .get([
+    //     param('id').isInt().withMessage('Id must be an integer')
+    // ], findOne)
+    .put((req, res, next) => {
+        passport.authenticate('jwt', { session: false }, async (err, user, info) => {
+            if (err) return internalServerError(err, req, res);
+            if (!user) return unAuthorized(req, res);
+            if (user.roleId === 1) return next();
+            const profile = await Profile.findByPk(req.query.id);
+            if (!profile) return notFound(req, res);
+            if (user.id !== req.body.id) return forbidden(req, res);
+            next();
+        })(req, res, next);
+    }, multer({ storage: profileStorage }).single('profilePicture'), [
+        param('id').isInt().withMessage('Id must be an integer'),
+        body('name')
+          .notEmpty()
+          .withMessage('Name is required')
+          .trim()
+          .isString()
+          .withMessage('Name must be a string'),
+        body('phoneNumber')
+          .notEmpty()
+          .withMessage('phoneNumber is required'),
+        // body('cityId')
+        //   .notEmpty()
+        //   .withMessage('cityId is required')
+        //   .trim()
+        //   .isInt()
+        //   .withMessage('cityId must be a integer'),
+        body('address')
+          .notEmpty()
+          .withMessage('address is required')
+          .trim()
+          .isString()
+          .withMessage('address must be a string'),
+    ], update)
+    .all(methodNotAllowed);
 
 router.post(
-  '/info-profile',
+  '/info-profiles',
   [
     body('name')
       .notEmpty()
@@ -30,7 +89,7 @@ router.post(
       .isString()
       .withMessage('address must be a string'),
   ],
-  updateProfile
+  update
 );
 
 module.exports = router;
