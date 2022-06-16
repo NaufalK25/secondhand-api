6d87c27af549d1cac320d3eed5acbfbbcb59bdba
LuthfiyahS <luthfiyah.sakinah19@gmail.com>
2022-06-16 10:05:02 +0700

    Test Error & Info Profile

---
+++ tests/error.spec.js
@@ -0,0 +1,103 @@
+const errorController = require('../controllers/error');
+
+const mockRequest = ({ method, originalName } = {}) => ({ method, originalName });
+const mockResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+}
+
+describe('errorController', () => {
+    test('400 Bad Request', () => {
+        const req = mockRequest();
+        const res = mockResponse();
+
+        errorController.badRequest([], req, res);
+
+        expect(res.status).toBeCalledWith(400);
+        expect(res.json).toBeCalledWith({
+            data: [],
+            message:"Validation error",
+            success:false
+        });
+    });
+    test('401 Unauthorized', () => {
+        const req = mockRequest();
+        const res = mockResponse();
+
+        errorController.unAuthorized(req, res);
+
+        expect(res.status).toBeCalledWith(401);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message:"Unauthorized",
+            success:false
+        });
+    });
+    test('403 Forbidden', () => {
+        const req = mockRequest();
+        const res = mockResponse();
+
+        errorController.forbidden(req, res);
+
+        expect(res.status).toBeCalledWith(403);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message:"Forbidden",
+            success:false
+        });
+    });
+    test('404 Not Found', () => {
+        const req = mockRequest({ originalName: '/api/v1/info-profiles' });
+        const res = mockResponse();
+
+        errorController.notFound(req, res);
+
+        expect(res.status).toBeCalledWith(404);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message: `Endpoint ${req.path} not found`,
+            success:false
+        });
+    });
+    test('405 Method Not Allowed', () => {
+        const req = mockRequest({ method: 'POST', originalName: '/api/v1/info-profile' });
+        const res = mockResponse();
+
+        errorController.methodNotAllowed(req, res);
+
+        expect(res.status).toBeCalledWith(405);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message: `Method ${req.method} not allowed at endpoint ${req.path}`,
+            success:false
+        });
+    });
+    test('500 Internal Server Error', () => {
+        const req = mockRequest();
+        const res = mockResponse();
+
+        errorController.internalServerError('Internal Server Error', req, res);
+
+        expect(res.status).toBeCalledWith(500);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message: 'Internal Server Error',
+            success:false
+        });
+    });
+    test('500 Internal Server Error with message', () => {
+        const req = mockRequest();
+        const res = mockResponse();
+
+        errorController.internalServerError(new Error('Internal Server Error'), req, res);
+
+        expect(res.status).toBeCalledWith(500);
+        expect(res.json).toBeCalledWith({
+            data: null,
+            message: 'Internal Server Error',
+            success:false
+        });
+     });
+})
