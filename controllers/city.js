d160bc5d5e356f7668cdc33b7f8043e1c591bffd
Busro Arshad <shandyabc313@gmail.com>
2022-06-17 20:22:52 +0700

    testing city

--- controllers/city.js
+++ controllers/city.js
@@ -3,10 +3,12 @@ const { City } = require('../models');
 
 module.exports = {
     getAll: async (req, res) => {
-        const city = await City.findAll({
+        const city = await City.findAll(
+            {
             attributes:["id","city"],
             order:[["id","ASC"]]
-         });
+         }
+         );
 
         res.status(200).json({
             success: true,
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,29 @@
+// const { Router } = require('express');
+// const authRouter = require('./auth');
+// const productRouter = require('./products');
+// const profileRouter = require('./profile');
+// const city = require('./city');
+
+// const router = Router();
+
+// router.use('/auth', authRouter);
+// router.use('/products', productRouter);
+// router.use(city);
+
+// module.exports = router;
+
 const { Router } = require('express');
 const authRouter = require('./auth');
-<<<<<<< HEAD
-const productRouter = require('./products');
-const profileRouter = require('./profile');
-const city = require('./city');
-=======
 const productRouter = require('./product');
 const userController = require('./user');
->>>>>>> 089f85f364ec26b2261161edf302e851aa1a0d5d
+const city = require('./city');
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/products', productRouter);
-<<<<<<< HEAD
-router.use(city);
-=======
 router.use('/user', userController);
+router.use(city);
 
->>>>>>> 089f85f364ec26b2261161edf302e851aa1a0d5d
 
 module.exports = router;
