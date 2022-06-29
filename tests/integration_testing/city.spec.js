d09c0b70fd114a8bde8a59107b6a3d9b95cb3f03
Busro Arshad <shandyabc313@gmail.com>
2022-06-29 20:45:32 +0700

    testing city

--- tests/integration_testing/city.spec.js
+++ tests/integration_testing/city.spec.js
@@ -1,12 +1,16 @@
 const request = require('supertest');
 const app = require('../../app');
+
 require('../../controllers/city');
+process.env.NODE_ENV = 'test';
+
 
 describe('GET /api/v1/city', () => {
-    it('200 OK', async () => {
-        const res = await request(app)
-            .get('/api/v1/cities')
-        expect(res.statusCode).toEqual(200);
-        expect(res.body.message).toEqual('Kota ditemukan');
+     it('200 OK', async () => {
+        const res = await request(app).get('/api/v1/cities')
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toEqual('Kota ditemukan')
+
     });
 });
+
