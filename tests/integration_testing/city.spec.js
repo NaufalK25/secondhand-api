56efdd26b8d77fbaf6158deeb4fa034736fd8461
NaufalK25 <naufalkateni2001@gmail.com>
2022-06-30 20:07:23 +0700

    Integration Test Auth

--- tests/integration_testing/city.spec.js
+++ tests/integration_testing/city.spec.js
@@ -1,16 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 
-require('../../controllers/city');
 process.env.NODE_ENV = 'test';
 
-
-describe('GET /api/v1/city', () => {
-     it('200 OK', async () => {
-        const res = await request(app).get('/api/v1/cities')
+describe('GET /api/v1/cities', () => {
+    test('200 OK', async () => {
+        const res = await request(app).get('/api/v1/cities');
         expect(res.statusCode).toBe(200);
-        expect(res.body.message).toEqual('Kota ditemukan')
-
+        expect(res.body.message).toEqual('Kota ditemukan');
     });
 });
-
