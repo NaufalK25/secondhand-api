4759fcc9701866cdc13ad16d295a3ee229ccd3ec
NaufalK25 <naufalkateni2001@gmail.com>
2022-07-07 19:47:50 +0700

    remove try catch

--- controllers/notification.js
+++ controllers/notification.js
@@ -9,28 +9,22 @@ const {
 
 module.exports = {
     findByUser: async (req, res) => {
-try {
-    const notification = await Notification.findAll({
-        where: { userId: req.user.id, status: false},
-        include: [
-            { model: Product, include: [{ model: ProductResource }]},
-            { model: ProductOffer}
-        ]
-    });
+        const notification = await Notification.findAll({
+            where: { userId: req.user.id, status: false },
+            include: [
+                { model: Product, include: [{ model: ProductResource }] },
+                { model: ProductOffer }
+            ]
+        });
 
-    if (notification.length === 0)
-        return notFound(req, res, 'Notifikasi tidak ditemukan');
+        if (notification.length === 0)
+            return notFound(req, res, 'Notifikasi tidak ditemukan');
 
-    res.status(200).json({
-        success: true,
-        message: 'Notifikasi ditemukan',
-        data: notification
-    });
-} catch (error) {
-    console.log('kesini ga',error);
-}
-
-        
+        res.status(200).json({
+            success: true,
+            message: 'Notifikasi ditemukan',
+            data: notification
+        });
     },
     update: async (req, res) => {
         const errors = validationResult(req);
