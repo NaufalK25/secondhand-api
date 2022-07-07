0b57a743da604a078342386fe5fe1f36a83e41ae
LuthfiyahS <luthfiyah.sakinah19@gmail.com>
2022-07-07 15:23:41 +0700

    fix bug notif

--- controllers/notification.js
+++ controllers/notification.js
@@ -4,27 +4,33 @@ const {
     Notification,
     Product,
     ProductOffer,
-    ProductResources
+    ProductResource
 } = require('../models');
 
 module.exports = {
     findByUser: async (req, res) => {
-        const notification = await Notification.findAll({
-            where: { userId: req.user.id, status: false },
-            include: [
-                { model: Product, include: [{ model: ProductResources }] },
-                { model: ProductOffer }
-            ]
-        });
+try {
+    const notification = await Notification.findAll({
+        where: { userId: req.user.id, status: false},
+        include: [
+            { model: Product, include: [{ model: ProductResource }]},
+            { model: ProductOffer}
+        ]
+    });
 
-        if (notification.length === 0)
-            return notFound(req, res, 'Notifikasi tidak ditemukan');
+    if (notification.length === 0)
+        return notFound(req, res, 'Notifikasi tidak ditemukan');
 
-        res.status(200).json({
-            success: true,
-            message: 'Notifikasi ditemukan',
-            data: notification
-        });
+    res.status(200).json({
+        success: true,
+        message: 'Notifikasi ditemukan',
+        data: notification
+    });
+} catch (error) {
+    console.log('kesini ga',error);
+}
+
+        
     },
     update: async (req, res) => {
         const errors = validationResult(req);
